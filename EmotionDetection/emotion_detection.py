2da97fbc7e6550fa13ea3e3dcc8bd67b4c827f3c
NerdMcDuck <NerdMcDuck@users.noreply.github.com>
2024-10-02 12:33:21 -0400

    Formatting and typo

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -1,25 +1,29 @@
 import json
 import requests
 
-def emotion_detector(text_to_analyze: str) -> str:
+
+def emotion_detector(text_to_analyze: str) -> dict:
     """
     Send a POST to Emotion Predict function of the Watson NLP Library with the given text
     """
 
     # URL of the Emotion Predict Service
-    url: str = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
-    
+    url: str = ('https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp'
+                '.v1/NlpService/EmotionPredict')
+
     # Set the headers required for the API request
-    header: dict = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    
+    header: dict = {
+        "grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+
     # Create a dictionary with the text to be analyzed
-    input_text: json = { "raw_document": { "text": text_to_analyze } }
-    
+    input_text: json = {"raw_document": {"text": text_to_analyze}}
+
     # Send a POST request to the API with the text and headers
     response = requests.post(url, json=input_text, headers=header, timeout=10)
 
     # return the text of the response
     return _extract_emotions(response)
+
 
 def _extract_emotions(response) -> dict:
     """
@@ -46,7 +50,7 @@ def _extract_emotions(response) -> dict:
     # Parse out the emotions dictionary
     emotions: dict = emotion_predictions[0]['emotion']
 
-    # Get the domincant emotion score
+    # Get the dominant emotion score
     dominant_emotion_score = max(list(emotions.values()))
 
     for emotion, score in emotions.items():
@@ -55,9 +59,3 @@ def _extract_emotions(response) -> dict:
             break
 
     return emotions
-
-
-
-
-
-
